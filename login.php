d4ed32060c7aafe7eec8653e1b74a87864a0c2ca
mrpandacollection <108386655+mrpandacollection@users.noreply.github.com>
2022-09-07 14:20:44 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mr.PandaCollection</title>
+	<link rel="stylesheet" type="text/css" href="design.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Finlandica:ital,wght@0,400;0,600;1,500;1,700&display=swap" rel="stylesheet">
+    <title>LogIn</title>
+<style>
+
+form {
+margin-left: 650px ;
+margin-top: 50px;
+margin-bottom:50px;
+width: 500px;
+padding: 20px;
+background: #000000;
+}
+
+h2 {
+text-align: center;
+margin-bottom: 40px;
+
+}
+
+input {
+display: block;
+border: 2px solid #ccc;
+width: 95%;
+padding: 10px;
+margin: 10px auto;
+border-radius: 5px;
+
+}
+
+label {
+color: #888;
+font-size: 20px;
+padding: 10px;
+margin-left:200px;
+}
+
+button {
+margin-left: 220px;
+border: none;
+font-size: 20px;
+}
+
+
+.error {
+background: #F2DEDE;
+color: #0c0101;
+padding: 10px;
+width: 95%;
+border-radius: 5px;
+margin: 20px auto;
+
+}
+
+h1 {
+text-align: center;
+color: #000000;
+}
+
+
+
+a:hover{
+opacity: .7;
+
+}
+
+</style>
+    <header>
+		<nav>
+			<a class="lg" href="index.php"><img src="images/logop.PNG" alt="logo"></a>
+				<div class="navigation">
+					<i class="menubar"> </i>
+					<ul>
+						<li> <a href="index.php">HOME</a></li>
+						<li> <a href="">ABOUT</a></li>
+						<li> <a href="">REQUEST</a></li>
+						<li> <a href="">DONATE</a></li>
+                        <li> <a href="signup.php">SIGN UP</a></li>
+					</ul>
+					<i class="menubar"> </i>
+				</div>
+		</nav>
+	</header>
+</head>
+<body>
+<form action="login.php" method="post">
+<h2>LOGIN</h2>
+<label>User Name</label>
+<input type="text" name="uname" placeholder="Inset Username"><br>
+<label>Password</label>
+<input type="password" name="password" placeholder="Insert Password"><br> 
+<button type="submit">Login</button>
+</form>
+<br>
+
+</body>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<footer>PLEASE DONATE FOR MORE KAHIT 1K LANG</footer>
+</html>
+
+
---
+++ movie.php
@@ -0,0 +1,291 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Mr.PandaCollection</title>	
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Finlandica:ital,wght@0,400;0,600;1,500;1,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="design.css">
+<style>
+
+	body {
+  font-family: Arial;
+  margin: 0;
+}
+
+* {
+  box-sizing: border-box;
+}
+
+img {
+  vertical-align: middle;
+}
+
+.container {
+  position: relative;
+}
+
+.mySlides {
+  display: none;
+}
+
+.cursor {
+  cursor: pointer;
+}
+
+/* Next & previous buttons */
+.prev {
+  cursor: pointer;
+  position: absolute;
+  top: 40%;
+  width: auto;
+  padding: 30px;
+  margin-top: -50px;
+  color: white;
+  font-weight: bold;
+  font-size: 20px;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+  -webkit-user-select: none;
+}
+.next {
+  cursor: pointer;
+  position: absolute;
+  top: 40%;
+  width: auto;
+  padding: 30px;
+  margin-top: -50px;
+  color: white;
+  font-weight: bold;
+  font-size: 20px;
+  border-radius: 0 3px 3px 0;
+  user-select: none;
+  -webkit-user-select: none;
+}
+
+/* Position the "next button" to the right */
+.next {
+  right: 0;
+  border-radius: 3px 0 0 3px;
+}
+
+/* On hover, add a black background color with a little bit see-through */
+.prev:hover,
+.next:hover {
+  background-color: rgba(0, 0, 0, 0.8);
+}
+
+/* Number text (1/3 etc) */
+.numbertext {
+  color: #f2f2f2;
+  font-size: 20px;
+  padding: 8px 12px;
+  position: absolute;
+  top: 0;
+}
+
+/* Container for image text */
+.caption-container {
+  text-align: center;
+  background-color: #ffffff;
+  padding: 2px 16px;
+  color: white;
+}
+
+.row:after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+/* Six columns side by side */
+.column {
+  float: left;
+  width: 16.66%;
+}
+
+/* Add a transparency effect for thumnbail images */
+.demo {
+  opacity: 0.6;
+}
+
+.active,
+.demo:hover ::container {
+  opacity: 1;
+}
+
+</style>
+</head>
+<body>
+<header>
+		<nav>
+			<a href="index.php"><img src="images/logop.PNG" alt="logo"></a>
+				<div class="navigation">
+					<i class="menubar"> </i>
+					<ul>
+						<li> <a href="index.php">HOME</a></li>
+						<li> <a href="">ABOUT</a></li>
+						<li> <a href="">REQUEST</a></li>
+						<li> <a href="">DONATE</a></li>
+					</ul>
+					<i class="menubar"> </i>
+				</div>
+		</nav>
+	</header>
+
+
+<!------COLLECTION---->
+	<section  class="boody">
+	<div class="movienav">
+<h3 class= "Movie" style="font-size: 35px;font-family: serif;"> ---------------MOVIES---------------<h3/>
+		<p style="color:white;">PASSWORD:megam0rph</p>
+<div class="container">
+	<div class="mySlides">
+		<div class="numbertext"> 1 / 3</div>
+
+	<a href="https://drive.google.com/file/d/1CbZeePBoN2A29TBn2_7EPlU7kbm6AR8T/view?usp=sharing">
+		<img  class="movim" border="0" alt="avengs" align="avengers" src="images/avend.jpg" width="300" height="450"></a>
+
+	<a href="https://drive.google.com/file/d/1Bp5Zgks3Q18wiYow5KWB1NpkEGXjNfio/view?usp=sharing">
+		<img class="movim" border="0" align = "doctor" src="images/ds.jpg" width="300" height="450"></a>
+
+	<a href="https://drive.google.com/file/d/1yFbiQvf8LYEDx5ZvkLOaQ3v2XVhSRMK1/view?usp=sharing">
+		<img class="movim" border="0" align="tr" src="images/turnr.jpg" width="300" height="450"></a>
+
+	<a href="https://www.mediafire.com/file/217ixrwdovkzx0q/Sonic22.mkv/file">
+		<img class="movim" border="0" align="tr" src="images/son.jpg" width="300" height="450"></a>
+	
+	<a href="https://www.mediafire.com/file/opbmnto1gc4gt2y/BLACK_WID.mkv/file">
+		<img class="movim" border="0" align="tr" src="images/bw.jpg" width="300" height="450"></a>
+
+	<a href="https://www.mediafire.com/file/lhs4v0hxk4mj4wb/REDN.mkv/file">
+		<img class="movim" border="0" align="tr" src="images/rn.jpg" width="300" height="450"></a>
+
+	<a href="https://uptobox.com/2lv4q3x177ni">
+		<img class="movim" border="0" align="tr" src="images/min.jpg" width="300" height="450"></a>
+
+			<a href="https://www.fireload.com/f863df19292fcf99/--=LEE=--Jurassic.World.Dominion.2022.HC.1080p.WEB-DL.Vega.x264.mkv">
+		<img class="movim" border="0" align="tr" src="images/jw.jpg" width="300" height="450"></a>
+
+			<a href="https://www.fireload.com/ccecc1a89ffe428a/--=LEE=--The.Princess.2022.1080p.HULU.WEBRip.1400MB.DD5.1.x264-GalaxyRG.mkv">
+		<img class="movim" border="0" align="tr" src="images/pr.jpg" width="300" height="450"></a>
+
+			<a href="https://www.mediafire.com/file/yiak3seizmho0la/Code.Name.Banshee.2022.1080p.WEBRip.1400MB.DD5.1.x264-GalaxyRG.mkv/file">
+		<img class="movim" border="0" align="tr" src="images/cb.png" width="300" height="450"></a>
+	</div>
+
+	<!----2----->
+		<div class="mySlides">
+		<div class="numbertext"> 2 / 3</div>
+
+	<a href="https://uptobox.com/l28r8qc7u0z3">
+		<img  class="movim" border="0" alt="avengs" align="avengers" src="images/mj.jpg" width="300" height="450"></a>
+
+	<a href="https://www.fireload.com/399c5a8cbb4ddbb0/--=LEE=--The.Passenger.2022.SPANISH.1080p.WEBRip.1400MB.DD5.1.x264-GalaxyRG.mkv">
+		<img class="movim" border="0" align = "doctor" src="images/psg.jpg" width="300" height="450"></a>
+
+	<a href="https://www.fireload.com/fe2f7f6a3a02b023/--=LEE=--The.Man.from.Toronto.2022.1080p.NF.WEBRip.1400MB.DD5.1.x264-GalaxyRG.mkv">
+		<img class="movim" border="0" align="tr" src="images/mft.jpg" width="300" height="450"></a>
+
+	<a href="https://www.mediafire.com/file/t13rcodsojaq423/UC%25282%2529.zip/file">
+		<img class="movim" border="0" align="tr" src="images/unc.jpg" width="300" height="450"></a>
+	
+	<a href="https://www.mediafire.com/file/qd30eoqvjgi9ypl/FBtSoD.zip/file">
+		<img class="movim" border="0" align="tr" src="images/fb.jpg" width="300" height="450"></a>
+
+	<a href="https://www.mediafire.com/file/k9hotsh98fgy1ax/TLC.zip/file">
+		<img class="movim" border="0" align="tr" src="images/tlc.jpg" width="300" height="450"></a>
+
+	<a href="https://www.mediafire.com/file/3cc8isja1vgfwib/Hus_tle.zip/file">
+		<img class="movim" border="0" align="tr" src="images/ht.jpg" width="300" height="450"></a>
+
+		<a href="https://www.mediafire.com/file/xedl10a8o6y4ss5/Memori.zip/file">
+		<img class="movim" border="0" align="tr" src="images/mem.png" width="300" height="450"></a>
+
+			<a href="https://www.mediafire.com/file/sghi8lyf0pv8wzv/uH.zip/file">
+		<img class="movim" border="0" align="tr" src="images/unh.jpg" width="300" height="450"></a>
+
+			<a href="https://www.mediafire.com/file/m6lol4dm925kg5c/EEAaO.zip/file">
+		<img class="movim" border="0" align="tr" src="images/eea.jpg" width="300" height="450"></a>
+	</div>
+<!----3----->
+		<div class="mySlides">
+		<div class="numbertext"> 3 / 3</div>
+
+	<a href="https://uptobox.com/l28r8qc7u0z3">
+		<img  class="movim" border="0" alt="avengs" align="avengers" src="images/mj.jpg" width="300" height="450"></a>
+
+	<a href="https://www.fireload.com/399c5a8cbb4ddbb0/--=LEE=--The.Passenger.2022.SPANISH.1080p.WEBRip.1400MB.DD5.1.x264-GalaxyRG.mkv">
+		<img class="movim" border="0" align = "doctor" src="images/psg.jpg" width="300" height="450"></a>
+
+	<a href="https://www.fireload.com/fe2f7f6a3a02b023/--=LEE=--The.Man.from.Toronto.2022.1080p.NF.WEBRip.1400MB.DD5.1.x264-GalaxyRG.mkv">
+		<img class="movim" border="0" align="tr" src="images/mft.jpg" width="300" height="450"></a>
+
+	<a href="https://www.mediafire.com/file/t13rcodsojaq423/UC%25282%2529.zip/file">
+		<img class="movim" border="0" align="tr" src="images/unc.jpg" width="300" height="450"></a>
+	
+	<a href="https://www.mediafire.com/file/qd30eoqvjgi9ypl/FBtSoD.zip/file">
+		<img class="movim" border="0" align="tr" src="images/fb.jpg" width="300" height="450"></a>
+
+	<a href="https://www.mediafire.com/file/k9hotsh98fgy1ax/TLC.zip/file">
+		<img class="movim" border="0" align="tr" src="images/tlc.jpg" width="300" height="450"></a>
+
+	<a href="https://www.mediafire.com/file/3cc8isja1vgfwib/Hus_tle.zip/file">
+		<img class="movim" border="0" align="tr" src="images/ht.jpg" width="300" height="450"></a>
+
+		<a href="https://www.mediafire.com/file/xedl10a8o6y4ss5/Memori.zip/file">
+		<img class="movim" border="0" align="tr" src="images/mem.png" width="300" height="450"></a>
+
+			<a href="https://www.mediafire.com/file/sghi8lyf0pv8wzv/uH.zip/file">
+		<img class="movim" border="0" align="tr" src="images/unh.jpg" width="300" height="450"></a>
+
+			<a href="https://www.mediafire.com/file/m6lol4dm925kg5c/EEAaO.zip/file">
+		<img class="movim" border="0" align="tr" src="images/eea.jpg" width="300" height="450"></a>
+	</div>
+
+</div>
+  <a class="prev" onclick="plusSlides(-1)">❮</a>
+  <a class="next" onclick="plusSlides(1)">❯</a>
+
+  <div class="caption-container">
+    <p id="caption"></p>
+  </div>
+</section>
+
+<!-----LIST SLIDE FUNCTION---->
+<script>
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+</script>
+    
+		<footer>PLEASE DONATE FOR MORE KAHIT 1K LANG</footer>
+</body>
+</html>
